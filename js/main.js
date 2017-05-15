056f912754b6b4550bfdfe55112fb1c6c6fc1c50
sergio gutierrez <sergioa.gutierrezs@gmail.com>
2017-05-15 15:54:49 -0500

    agregada funcion para buscar actividades pendientes agregada funcion para agregar actividades a la lista

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
+var url = "https://jsonplaceholder.typicode.com";
+var errorServer = "Ocurrio un error consultando los datos del servidor";
+
 $(document).ready(function(){
+	$(".logo_carga").addClass("active");
+
+	$.ajax({
+		url: url + "/users",
+		method: 'GET',
+		success: function(data){
+			var datos = JSON.parse(JSON.stringify(data));
+			var i = 0;
+			var html = "";
+			while (datos[i]) {
+				html += '<li class="list_element"> ' + datos[i].name +  ': ' + datos[i].username + ' <hr class="vertical red"/></li>';
+				i++;
+			}
+			$("#tareas_pendientes").html(html);
+			$(".logo_carga").removeClass("active");
+			
+		},
+		error: function(){
+			alert(errorServer);
+			$(".logo_carga").removeClass("active");
+		}
+	});
+
 	$(".close_dialog").click(function(){
 		$(".modal_background").removeClass("active");
 		$(".modal_box").removeClass("active");
@@ -11,9 +37,13 @@ $(document).ready(function(){
 
 	$("#crear_actividad").click(function(){
 		if ($('#descripcion_actividad').val() == "") {
-			alert("NO");
+			alert("Debe ingresar la descripcion de la tarea para continuar");
 		} else {
-			alert("SI");
+			var html = '<li class="list_element"> ' + $('#descripcion_actividad').val() + ' <hr class="vertical red"/></li>';
+			$("#tareas_pendientes").append(html);
+			$('#descripcion_actividad').val("");
+			$(".modal_background").removeClass("active");
+			$(".modal_box").removeClass("active");
 		}
 	});
 });
