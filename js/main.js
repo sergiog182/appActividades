720ac812d102356e63f29088bde5cf39f16893db
Sergio Gutierrez <sergioa.gutierrezs@gmail.com>
2017-05-27 22:52:45 -0500

    Cambios en el codigo para gestionar actividades

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-//var url = "http://25.8.54.211/WsActividades/";
-//var url = "https://jsonplaceholder.typicode.com";
-var url = "http://localhost/WsActividades/";
+var url = "http://25.8.54.211/WsActividades/";
 var errorServer = "Ocurrio un error consultando los datos del servidor";
 
 function test(data)
@@ -16,10 +14,8 @@ $(document).ready(function(){
 		//url: url + "/users",
 		method: 'GET',
 		dataType: 'JSON',
-		success: function(data){
-			alert("Aqui");
-			var datos = JSON.parse(data);
-			//var datos = JSON.parse(JSON.stringify(data));
+		success: function(data) {
+			var datos = JSON.parse(JSON.stringify(data));
 			var i = 0;
 			var html = "";
 			var color = "";
@@ -36,7 +32,7 @@ $(document).ready(function(){
 					default: color = "white";
 							break;
 				}
-				html += '<li class="list_element" data-actividad="' + datos[i].IdActividad + '"> ' + datos[i].Descripcion + ' <hr class="vertical ' + color + '"/></li>';
+				html += '<li class="list_element" data-actividad="' + datos[i].IdActividad + '" data-clasificacion="' + datos[i].IdClasificacion + '" data-fechaEjecucion="' + datos[i].FechaEjecucion + '"> ' + datos[i].Descripcion + ' <hr id="vertical_' + datos[i].IdActividad + '" class="vertical ' + color + '"/></li>';
 				i++;
 			}
 			$("#tareas_pendientes").html(html);
@@ -66,55 +62,102 @@ $(document).ready(function(){
 	});
 
 	$("#crear_actividad").click(function(){
-		if ($('#descripcion_actividad').val() == "") {
+		var textoDescripcion = $('#descripcion_actividad').val();
+		if (textoDescripcion == "") {
 			alert("Debe ingresar la descripcion de la tarea para continuar");
 		} else {
-			/*$.ajax({
-				url: url + "/AgregarActividad",
+			$(".logo_carga").addClass("active");
+			$.ajax({
+				url: url + "Actividades",
 				method: 'POST',
-				data: {"Descripcion": "Tarea de Prueba", "FechaEjecucion": "", "IdActividad": "1", "IdClasificacion": "1", "IdEstado": "1", "IdUsuario": "1"},
+				contentType: "application/json; charset=utf-8",
+				data: {"Descripcion": textoDescripcion, "FechaEjecucion": "", "IdActividad": 0, "IdClasificacion": "", "IdEstado": "", "IdUsuario": "1"},
 				success: function(data){
-					var datos = JSON.parse('');
-					var i = 0;
-					var html = "";
-					var color = "";
-					while (datos[i]) {
-						switch(datos[i].IdClasificacion) {
-							case "1": color = "red";
-									break;
-							case "2": color = "green";
-									break;
-							case "3": color = "yellow";
-									break;
-							case "4": color = "blue";
-									break;
-							default: color = "white";
-									break;
-						}
-						html += '<li class="list_element"> ' + datos[i].Descripcion + ' <hr class="vertical ' + color + '"/></li>';
-						i++;
-					}
-					$("#tareas_pendientes").html(html);
+					var datos = JSON.parse(JSON.stringify(data));
+					var html = '<li class="list_element" data-actividad="' + datos.IdActividad + '" data-clasificacion="0" data-fechaEjecucion=""> ' + $('#descripcion_actividad').val() + ' <hr id="vertical_' + datos.IdActividad + '" class="vertical white"/></li>';
+					$("#tareas_pendientes").append(html);
+					$('#descripcion_actividad').val("");
 					$(".logo_carga").removeClass("active");
-					
+					$(".modal_background").removeClass("active");
+					$(".modal_box").removeClass("active");
 				},
 				error: function(){
 					alert(errorServer);
 					$(".logo_carga").removeClass("active");
 				}
-			});*/
-			var html = '<li class="list_element"> ' + $('#descripcion_actividad').val() + ' <hr class="vertical white"/></li>';
-			$("#tareas_pendientes").append(html);
-			$('#descripcion_actividad').val("");
-			$(".modal_background").removeClass("active");
-			$(".modal_box").removeClass("active");
+			});
 		}
 	});
 
 	$(document).on("click", ".list_element", function(){
 		var actividad = $(this).data("actividad");
+		var clasificacion = $(this).data("clasificacion") | 0;
+		var fechaEjecucion = $(this).data("fechaEjecucion");
+		var descripcionActividad = $(this).html();
+
+		console.log(descripcionActividad);
+
+		$('#div_descripcion').html(descripcionActividad);
+		$('#idActividadGestionar').val(actividad);
+		$('#cmbCalificacion').val(clasificacion);
+		$('#fecha_ejecucion').val(fechaEjecucion);
+
 		$("#gestionar_tarea").addClass("active");
 		$(".modal_background").addClass("active");
+	});
+
+	$("#actualizar_actividad").click(function(){
+		var descripcion = $('#div_descripcion').innerHTML;
+		var actividad = $('#idActividadGestionar').val();
+		var calificacion = $('#cmbCalificacion').val();
+		var fechaEjecucion = $('#fecha_ejecucion').val();
+
+		switch(calificacion) {
+			case "1": color = "red";
+					break;
+			case "2": color = "green";
+					break;
+			case "3": color = "yellow";
+					break;
+			case "4": color = "blue";
+					break;
+			default: color = "white";
+					break;
+		}
+		console.log('{"Descripcion": "' + descripcion + '", "FechaEjecucion": "' + fechaEjecucion + '", "IdActividad": "' + actividad + '", "IdClasificacion": "' + calificacion + '", "IdEstado": "1", "IdUsuario": "1"}');
+		document.getElementById("vertical_" + actividad).className = "vertical " + color;
+		$(".logo_carga").removeClass("active");
+		$(".modal_background").removeClass("active");
+		$(".modal_box").removeClass("active");
+		/*
+		$.ajax({
+			url: url + "Actividades",
+			method: 'PUT',
+			data: {"Descripcion": descripcion, "FechaEjecucion": fechaEjecucion, "IdActividad": actividad, "IdClasificacion": calificacion, "IdEstado": "1", "IdUsuario": "1"},
+			success: function(data){
+				var color = "";
+				switch(calificacion) {
+					case "1": color = "red";
+							break;
+					case "2": color = "green";
+							break;
+					case "3": color = "yellow";
+							break;
+					case "4": color = "blue";
+							break;
+					default: color = "white";
+							break;
+				}
+				document.getElementById("vertical_" + actividad).className = "vertical " + color;
+				$(".logo_carga").removeClass("active");
+				$(".modal_background").removeClass("active");
+				$(".modal_box").removeClass("active");
+			},
+			error: function(){
+				alert(errorServer);
+				$(".logo_carga").removeClass("active");
+			}
+		});*/
 	});
 
 });
